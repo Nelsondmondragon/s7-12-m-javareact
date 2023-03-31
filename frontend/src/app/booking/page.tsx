c02b4f5214de946975734810bdb77cf887e98e10
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-03-31 12:44:44 -0500

    add object

--- frontend/src/app/booking/page.tsx
+++ frontend/src/app/booking/page.tsx
@@ -11,6 +11,9 @@ export default function Booking() {
   const [startTime, setstartTime] = useState(new Date());
   const [endTime, setEndTime] = useState(new Date());
   const [category, setCategory] = useState("");
+  const [startPl, setStartPl] = useState()
+  const [returnPl, setReturnPl] = useState()
+
   registerLocale("es", es);
 
   const categories = [
@@ -34,81 +37,101 @@ export default function Booking() {
     },
   ];
 
-const onCategory = (cat) => {
-  setCategory(cat.id)
-  console.log(cat.id);
-  
-}
+  const onCategory = (cat) => {
+    setCategory(cat.name);
+    console.log(cat.name);
+  };
 
-const onSearch = () => {
+  const onSearch = () => {
+    
+    const selection = {
+      startPlace: startPl,
+      startDate: startDate,
+      startTime: startTime,
+      returnPlace: returnPl,
+      endDate: endDate,
+      endtTime: endTime,
+      category: category
+    }
+    
+    console.log(selection);
+    
 
-}
+  };
 
   return (
     <section className="min-h-screen flex flex-col">
       <h2 className="text-3xl"> RESERVAS </h2>
-      <div className="flex">
-        <input className="border shadow-lg" type="text" />
-        <DatePicker
-          className="border"
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          selectsStart
-          startDate={startDate}
-          endDate={endDate}
-          locale="es"
-          dateFormat="d MMMM, yyyy"
-        />
-        <DatePicker
-          selected={startTime}
-          onChange={(date) => setstartTime(date)}
-          showTimeSelect
-          showTimeSelectOnly
-          timeIntervals={15}
-          timeCaption="Hora"
-          dateFormat="h:mm aa"
-        />
-      </div>
-      <div className="flex">
-        <input className="border shadow-lg" type="text" />
-        <DatePicker
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-          selectsEnd
-          startDate={startDate}
-          endDate={endDate}
-          minDate={startDate}
-          locale="es"
-          dateFormat="d MMMM, yyyy"
-        />
-        <DatePicker
-          selected={endTime}
-          onChange={(date) => setEndTime(date)}
-          showTimeSelect
-          showTimeSelectOnly
-          timeIntervals={15}
-          timeCaption="Hora"
-          dateFormat="h:mm aa"
-          locale="es"
-        />
-      </div>
-      <div>
-        <div className="flex">
-          {categories.map((cat) => (
-            <div
-              key={cat.id}
-              className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8"
-              onClick={()=> onCategory(cat) }
-            >
-              <div className="flex flex-col w-36 gap-2" >
-                <img src={cat.img} alt="" />
-                <h3>{cat.name}</h3>
-                <p>{cat.desc}</p>
-              </div>
-            </div>
-          ))}
+      <h3>Consulta disponibilidad y reserva en línea al instante</h3>
+      <div className=" mx-auto mt-3">
+        <div className="flex mb-1 gap-2 justify-center">
+          <input className="border shadow-lg" type="text" placeholder="Lugar" onChange={e => setStartPl(e.target.value)} />
+          <DatePicker
+            className=" bg-blue-700 text-white text-center rounded-md w-40"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            locale="es"
+            dateFormat="d MMMM, yyyy"
+          />
+          <DatePicker
+          className=" bg-blue-700 text-white text-center rounded-md w-40"
+            selected={startTime}
+            onChange={(date) => setstartTime(date)}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            timeCaption="Hora"
+            dateFormat="h:mm aa"
+          />
         </div>
-        <button onClick={onSearch} className=" bg-slate-400 rounded-md px-2">Busqueda</button>
+        <div className="flex gap-2">
+          <input className="border shadow-lg" type="text" placeholder="Lugar" onChange={e=>setReturnPl(e.target.value)}/>
+          <DatePicker
+          className=" bg-blue-700 text-white text-center rounded-md w-40"
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            locale="es"
+            dateFormat="d MMMM, yyyy"
+          />
+          <DatePicker
+          className=" bg-blue-700 text-white text-center rounded-md w-40"
+            selected={endTime}
+            onChange={(date) => setEndTime(date)}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            timeCaption="Hora"
+            dateFormat="h:mm aa"
+            locale="es"
+          />
+        </div>
+        <div>
+          <div className="flex">
+            {categories.map((cat) => (
+              <div
+                key={cat.id}
+                className={`relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8 hover:border-blue-700 ${category=== cat.name && 'border-red-700'}`}
+                onClick={() => onCategory(cat)}
+              >
+                <div className="flex flex-col w-36 gap-2 ">
+                  <img src={cat.img} alt="" />
+                  <h3>{cat.name}</h3>
+                  <p>{cat.desc}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <button onClick={onSearch} className=" bg-slate-400 rounded-md px-2">
+            Busqueda
+          </button>
+        </div>
       </div>
     </section>
   );
