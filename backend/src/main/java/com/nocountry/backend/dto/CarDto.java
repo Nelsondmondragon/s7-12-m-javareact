9ac7279dc1279e7e934b51ba0ede4a51b6a4273e
ArielRzz <99253027+ArielRzz@users.noreply.github.com>
2023-03-31 01:41:48 -0300

    added CarDTO

--- backend/src/main/java/com/nocountry/backend/dto/CarDto.java
+++ backend/src/main/java/com/nocountry/backend/dto/CarDto.java
@@ -1,5 +1,32 @@
 package com.nocountry.backend.dto;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarDto {
 
+    private Long id;
+
+    private String image;
+
+    private String model;
+
+    private String make;
+
+    private int year;
+
+    private boolean air;
+
+    private boolean gps;
+
+    private Integer passengers;
+
+    private String patent;
+
+    private boolean Available;
 }
