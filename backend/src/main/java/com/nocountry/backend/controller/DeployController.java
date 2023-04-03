5d587404016c294529ed8b1da73afae9bbff3bda
Nelson M <nelsondmondragon@gmail.com>
2023-04-03 12:12:33 -0500

    deploy temp backend

---
+++ backend/src/main/java/com/nocountry/backend/controller/DeployController.java
@@ -0,0 +1,16 @@
+package com.nocountry.backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class DeployController {
+
+    @GetMapping("/deploy")
+    private String deploy() {
+        return "Holi";
+    }
+}
