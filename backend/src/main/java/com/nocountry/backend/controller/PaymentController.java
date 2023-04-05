5df72478db791fcaa30e6c01a734444a57a981f7
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-04-05 11:13:27 -0300

    add PaymentController component

--- backend/src/main/java/com/nocountry/backend/controller/PaymentController.java
+++ backend/src/main/java/com/nocountry/backend/controller/PaymentController.java
@@ -1,5 +1,11 @@
 package com.nocountry.backend.controller;
 
+import com.nocountry.backend.dto.PaymentDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PaymentController {
 
+    @Autowired
     private final IPaymentService service;
+
+    @PostMapping("/pay")
+    public ResponseEntity<String> charge(@RequestBody PaymentDto paymentDto) throws Exception {
+        String token = service.createToken(paymentDto);
+        String chargeId = service.createCharge(paymentDto, token);
+        return new ResponseEntity<>(chargeId, HttpStatus.OK);
+    }
+
 }
+
+
+
