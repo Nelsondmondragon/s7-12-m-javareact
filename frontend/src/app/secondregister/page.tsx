0bf36a48df5fa7264ac9848a2e9e6da061e7f32b
Eitan <eitan03@hotmail.com.ar>
2023-04-11 01:05:56 -0300

    changes on register and secondregister

--- frontend/src/app/secondregister/page.tsx
+++ frontend/src/app/secondregister/page.tsx
@@ -3,6 +3,7 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import vector from './vector.svg';
+import { useRouter } from 'next/navigation';
 // import { Schema as schema } from './registerValidation';
 
 // type FormValues = {
@@ -14,6 +15,10 @@ import vector from './vector.svg';
 // };
 
 const Register2 = () => {
+  
+  const router = useRouter();
+  
+  const irAtras = () => router.push("register")
   //   const {
   //     register,
   //     handleSubmit,
@@ -38,98 +43,101 @@ const Register2 = () => {
   //   };
 
   return (
-    <div className="w-full bg-mobile-pattern md:bg-global-pattern bg-no-repeat bg-cover bg-center ">
-      <form className="px-48 py-24">
-        <div>
-          <h2 className="text-4xl pb-4">Ya casi terminamos...</h2>
-          <div className="flex">
-            {/* <img src={vector} /> */}
-            <p className="text-xl pb-3">
-              Te pediremos algunos datos mas para completar tu perfil
-            </p>
-          </div>
+    <div className="register flex justify-center px-10 py-11 bg-mobile-pattern md:bg-global-pattern bg-no-repeat bg-cover bg-center ">
+      <div className='rounded-[20px] bg-form backdrop-blur-[8px]'>
+        <div className='px-16 pt-16'>
+          <h2 className="text-4xl pb-4">Datos de contacto</h2>
         </div>
-        <div className="pb-10">
-          <div className="flex flex-col pb-2">
-            <label htmlFor="name" className="font-semibold pb-2">
-              Direccion
-            </label>
-            <input className="w-2/4 rounded-md h-11 bg-slate-200 outline-none px-2 py-4" />
-            {/* <p className="text-red-600 text-sm font-bold">
+        <form className="px-16 pb-14 ">
+          <div className='flex'>
+            <div className='pr-10'>
+              <div className="flex flex-col pb-2">
+                <label htmlFor="name" className="font-semibold pb-2">
+                  Nombre
+                </label>
+                <input className="w-96 rounded-md h-11 bg-[#FFFFFF] outline-none px-2 py-4 border-[#77CEFF] border-solid border" />
+                {/* <p className="text-red-600 text-sm font-bold">
               {errors?.name?.message}
             </p> */}
-          </div>
-          <div className="flex flex-col pb-2">
-            <label htmlFor="name" className="font-semibold pb-2">
-              DNI
-            </label>
-            <input className="w-2/4 rounded-md h-11 bg-slate-200 outline-none px-2 py-4" />
-            {/* <p className="text-red-600 text-sm font-bold">
+              </div>
+              <div className="flex flex-col pb-2">
+                <label htmlFor="lastName" className="font-semibold pb-2">
+                  Apellido
+                </label>
+                <input className="w-96 rounded-md h-11 bg-[#FFFFFF] outline-none px-2 py-4 border-[#77CEFF] border-solid border" />
+                {/* <p className="text-red-600 text-sm font-bold">
               {errors?.name?.message}
             </p> */}
-          </div>
-          <div className="flex flex-col pb-2">
-            <label htmlFor="name" className="font-semibold pb-2">
-              N° Licencia de conducir
-            </label>
-            <input className="w-2/4 rounded-md h-11 bg-slate-200 outline-none px-2 py-4" />
-            {/* <p className="text-red-600 text-sm font-bold">
+              </div>
+              <div className="flex flex-col pb-2">
+                <label htmlFor="email" className="font-semibold pb-2">
+                  E-mail
+                </label>
+                <input className="w-96 rounded-md h-11 bg-[#FFFFFF] outline-none px-2 py-4 border-[#77CEFF] border-solid border" />
+                {/* <p className="text-red-600 text-sm font-bold">
               {errors?.name?.message}
             </p> */}
-          </div>
-        </div>
-        <div>
-          <h2 className="text-xl pb-5">Información de pago</h2>
-          <p className="text-xl pb-10">
-            Esta sección podrás completarla ahora, o mas tarde. <br />
-            <br />
-            <b>
-              Recorda que para realizar una reserva, deberás presentar una
-              tarjeta de crédito a tu nombre
-            </b>
-          </p>
-        </div>
-        <div className="w-3/5">
-          <div className="flex flex-col pb-2">
-            <label htmlFor="name" className="font-semibold pb-2">
-              N° de tarjeta
-            </label>
-            <input className="w-full rounded-md h-11 bg-slate-200 outline-none px-2 py-4" />
-            {/* <p className="text-red-600 text-sm font-bold">
+              </div>
+              <div className="flex flex-col pb-2">
+                <label htmlFor="city" className="font-semibold pb-2">
+                  Ciudad
+                </label>
+                <input className="w-96 rounded-md h-11 bg-[#FFFFFF] outline-none px-2 py-4 border-[#77CEFF] border-solid border" />
+                {/* <p className="text-red-600 text-sm font-bold">
               {errors?.name?.message}
             </p> */}
-          </div>
-          <div className="flex flex-col pb-2">
-            <label htmlFor="name" className="font-semibold pb-2">
-              Nombre y apellido (tal como figura en la tarjeta)
-            </label>
-            <input className="w-full rounded-md h-11 bg-slate-200 outline-none px-2 py-4" />
-            {/* <p className="text-red-600 text-sm font-bold">
-              {errors?.name?.message}
-            </p> */}
-          </div>
-          <div className="flex ">
-            <div className="flex flex-col pb-2 w-1/2">
-              <label htmlFor="name" className="font-semibold pb-2">
-                Vencimiento
-              </label>
-              <input className="w-full rounded-md h-11 bg-slate-200 outline-none px-2 py-4" />
-              {/* <p className="text-red-600 text-sm font-bold">
-              {errors?.name?.message}
-            </p> */}
+              </div>
             </div>
-            <div className="flex flex-col pb-2 w-1/2">
-              <label htmlFor="name" className="font-semibold pb-2">
-                CVV
-              </label>
-              <input className="w-full rounded-md h-11 bg-slate-200 outline-none px-2 py-4" />
-              {/* <p className="text-red-600 text-sm font-bold">
+
+            <div>
+              <div className="flex flex-col pb-2">
+                <label htmlFor="adress" className="font-semibold pb-2">
+                  Direccion
+                </label>
+                <input className="w-96 rounded-md h-11 bg-[#FFFFFF] outline-none px-2 py-4 border-[#77CEFF] border-solid border" />
+                {/* <p className="text-red-600 text-sm font-bold">
               {errors?.name?.message}
             </p> */}
+              </div>
+              <div className="flex flex-col pb-2">
+                <label htmlFor="dni" className="font-semibold pb-2">
+                  DNI
+                </label>
+                <input className="w-96 rounded-md h-11 bg-[#FFFFFF] outline-none px-2 py-4 border-[#77CEFF] border-solid border" />
+                {/* <p className="text-red-600 text-sm font-bold">
+              {errors?.name?.message}
+            </p> */}
+              </div>
+              <div className="flex flex-col pb-2">
+                <label htmlFor="licence" className="font-semibold pb-2">
+                  N° Licencia conducir
+                </label>
+                <input className="w-96 rounded-md h-11 bg-[#FFFFFF] outline-none px-2 py-4 border-[#77CEFF] border-solid border" />
+                {/* <p className="text-red-600 text-sm font-bold">
+              {errors?.name?.message}
+            </p> */}
+              </div>
+              <div className="flex flex-col pb-2">
+                <label htmlFor="name" className="font-semibold pb-2">
+                  Fecha de nacimiento
+                </label>
+                <input className="w-96 rounded-md h-11 bg-[#FFFFFF] outline-none px-2 py-4 border-[#77CEFF] border-solid border" />
+                {/* <p className="text-red-600 text-sm font-bold">
+              {errors?.name?.message}
+            </p> */}
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+
+          <div className='flex justify-between pt-11'>
+            <button className="py-2 px-7 mt-4 w-48 bg-[#FAFAFA] text-base text-[#03649B] rounded-md justify-self-end font-bold" type='button' onClick={irAtras}> Volver
+            </button>
+
+            <button className="py-2 px-7 mt-4 w-48 bg-[#03649B] text-base text-[#D2EEFE] rounded-md self-end" type="submit"> Guardar y Avanzar
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
