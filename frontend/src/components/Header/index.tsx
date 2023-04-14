2964eec4abf3cfd58d166e731611257f815af925
TaguaraDigital <taguaradigital@hotmail.com>
2023-04-14 19:34:35 -0400

    mejora en la presentacion de caputura de datos del cliente

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,12 +10,7 @@ import Cookies from 'js-cookie';
 import logo from '../../../public/assets/images/logos/logo.png';
 import logoMobile from '../../../public/assets/images/logos/logo-mobile.png';
 import logoTablet from '../../../public/assets/images/logos/logo-tablet.png';
-import { ModalLogin } from '../login/ModalLogin';
-import {
-  openModalLogin,
-  removeUser,
-  selectCurrentUser,
-} from '@/features/users/userSlice';
+import { removeUser, selectCurrentUser } from '@/features/users/userSlice';
 
 const links = [
   {
@@ -66,6 +61,7 @@ export const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [showModalPerfil, setShowModalPerfil] = useState(false);
   const currentUser = useSelector(selectCurrentUser);
+
   const dispatch = useDispatch();
   const router = useRouter();
 
@@ -80,19 +76,21 @@ export const Header = () => {
     localStorage.removeItem('user');
     localStorage.removeItem('category');
     setShowModalPerfil(false);
-    console.log('pulso logout');
     router.push('/');
   };
 
   const handleLogin = () => {
-    dispatch(openModalLogin());
+    router.push('/login');
   };
+
   const handleRegister = () => {
     router.push('/newregister');
   };
+
   const handleOpenModalProfile = () => {
     setShowModalPerfil(true);
   };
+
   const handleProfile = () => {
     setShowModalPerfil(false);
     router.push('/profile');
@@ -126,13 +124,13 @@ export const Header = () => {
 
         <nav className="flex lg:flex-row-reverse justify-between items-center">
           <div>
-            {currentUser.firstName !== '' ? (
+            {currentUser.fullName ? (
               <div className="flex space-x-2 ">
                 <button
                   className="w-8 h-8 md:w-[40px] md:h-[40px] lg:w-[60px] lg:h-[60px]  md:ml-12 bg-primary-200 hover:bg-primary-300 rounded-full flex justify-center items-center text-base md:text-xl"
                   onClick={() => handleOpenModalProfile()}
                 >
-                  {`${currentUser.firstName[0].toUpperCase()}${currentUser.lastName[0].toUpperCase()}`}
+                  {currentUser.fullName[0].toUpperCase()}
                 </button>
               </div>
             ) : (
@@ -189,9 +187,8 @@ export const Header = () => {
           </ul>
         </nav>
       </header>
-      {currentUser.firstName === '' && <ModalLogin />}
       {showModalPerfil && (
-        <div className="absolute bg-primary-700 w-36 h-20  top-[var(--header-heigth)] right-10 z-50 p-4 text-white flex flex-col">
+        <div className="absolute bg-primary-700 w-36 h-20  top-[var(--header-heigth)] right-10 z-40 p-4 text-white flex flex-col">
           <button
             className="text-left font-semibold cursor-pointer"
             onClick={handleProfile}
