76577ec2f31f3160782d08e276f295fef6cf44cb
Nelson M <nelsondmondragon@gmail.com>
2023-04-13 23:18:06 -0500

    add openapidefinition

--- backend/src/main/java/com/nocountry/backend/config/SpringDocConfig.java
+++ backend/src/main/java/com/nocountry/backend/config/SpringDocConfig.java
@@ -1,11 +1,19 @@
 package com.nocountry.backend.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
+@OpenAPIDefinition(servers = {@Server(url = "https://{host}",
+        variables = @ServerVariable(name = "host",
+                defaultValue = "s7-12-m-javareact-production.up.railway.app",
+                allowableValues = {"s7-12-m-javareact-production.up.railway.app", "hostname_uat"}),
+        description = "Host name by environment")})
 @SecurityScheme(type = SecuritySchemeType.HTTP, name = "bearerAuth", scheme = "bearer", bearerFormat = "JWT")
 public class SpringDocConfig {
 
