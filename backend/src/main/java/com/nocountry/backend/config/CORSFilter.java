106ff4ced8ed47160aafc100fbb38ff3c269b0da
Nelson M <nelsondmondragon@gmail.com>
2023-04-16 09:55:40 -0500

    update config cors

--- backend/src/main/java/com/nocountry/backend/config/CORSFilter.java
+++ backend/src/main/java/com/nocountry/backend/config/CORSFilter.java
@@ -7,20 +7,28 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
+
+
+        res.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS");
-        res.setHeader("Access-Control-Max-Age", "36000");
-        res.setHeader("Access-Control-Allow-Headers", "*");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
         res.setHeader("Cache-Control", "no-cache, no-store, max-age=36000");
         chain.doFilter(request, response);
     }
